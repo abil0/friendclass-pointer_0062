154339dedb69c2d33aea4e8f92519152b850bd5b
abil0 <hoenabil6@gmail.com>
2026-05-05 10:23:25 +0700

    membuat void dan input main

--- friendfunctionsatu.cpp
+++ friendfunctionsatu.cpp
@@ -10,3 +10,15 @@ public :
      friend void setnama (mahasiswa &a, string);
 };
 
+void setnama(mahasiswa&a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+
+int main()
+{
+    mahasiswa joko;
+    setnama(joko,"kairi kumar");
+    return 0;
+}
