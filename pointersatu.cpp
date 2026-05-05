4aafdf92b6aacf3589e5a65007b21ab55192b8e0
abil0 <hoenabil6@gmail.com>
2026-05-05 10:26:31 +0700

    input main

--- pointersatu.cpp
+++ pointersatu.cpp
@@ -10,3 +10,17 @@ class mahasiswa{
 };
 
 
+int main (){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); // member access operator
+
+
+    mahasiswa &reffMhs = mhs; //pointer reference reffMhs
+    reffMhs.nim = 2; //member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // pointer dereference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;
+}
